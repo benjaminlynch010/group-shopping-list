0dc7b0cc5330a5a0fc7bf80142bc47f7afb4f656
Ben Lynch <bdlynch7@gmail.com>
2023-08-11 11:08:34 -0500

    added sql db

--- database.sql
+++ database.sql
@@ -1,2 +1,13 @@
 -- Don't forget to add your create table SQL 
--- It is also helpful to include some test data
+-- It is also helpful to include some test dataCREATE TABLE list (
+	"id" SERIAL PRIMARY KEY,
+	"item" varchar(80),
+	"quantity" DECIMAL(5,2),
+	"unit" varchar(20),
+	"purchased" boolean not null default false
+);
+
+INSERT INTO list ("item", "quantity", "unit")
+VALUES ('Steak', '4', 'pounds');
+
+DROP TABLE list;
